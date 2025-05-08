47cff4d081f0f64c7fe31f5684706e73ab70e992
John Hole <giovannibuco2020@gmail.com>
2025-05-08 18:56:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Funzione globale per aprire il trailer
+function openTrailer() {
+    document.getElementById('trailerModal').style.display = 'block';
+  }
+  
+  // Funzione globale per chiudere il trailer
+  function closeTrailer() {
+    document.getElementById('trailerModal').style.display = 'none';
+  }
+  
+  // Funzione per il download protetto da password
+  function downloadTrailer() {
+    const password = prompt("Inserisci la password per scaricare il trailer:");
+    if (password === "cryptoD") {
+      window.open("https://drive.google.com/file/d/1PoGHrBkkJdqW4Z8ZhLeUrtbClo1p-ngx/view?usp=sharing ", "_blank");
+    } else {
+      alert("Password errata!");
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
     const langToggle = document.getElementById("langToggle");
     let isItalian = false;
   
